493fe6e57070dc5b671ba0465431e2a973c67a67
Eirik S. Morland <eirik@morland.no>
2015-04-25 20:17:55 +0200

    Add test coverage and make it 100%.

--- test/test.js
+++ test/test.js
@@ -6,30 +6,69 @@ var helpers = require('yeoman-generator').test;
 var os = require('os');
 var fs = require('fs');
 
+var testDependency = 'test' + Math.random();
+var dependencyAdded = false;
+var dependencyPropTransformer = function(prop) {
+  if (dependencyAdded) {
+    return {dependency: ''};
+  }
+  dependencyAdded = true;
+  return prop;
+}
+
 var mockPrompts = {
   moduleName: 'testModule',
   moduleDesc: 'Test creation of module',
   install: true,
-  hooks: [false, false, true, false]
+  hooks: ['theme'],
+  dependency: testDependency
 };
-
+var opts = { dependencyPropTransformer: dependencyPropTransformer };
 
 describe('yeoman:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .withOptions({ 'skip-install': true })
-      .withPrompt(mockPrompts)
-      .on('end', done);
-  });
+  var runGenerator = function(callback) {
+      helpers.run(path.join(__dirname, '../app'))
+        .inDir(path.join(os.tmpdir(), './temp-test'))
+        .withOptions(opts)
+        .withPrompt(mockPrompts)
+        .on('end', callback);
+    };
 
   // Test to see if all files are created.
-  it('creates files and folders', function () {
-    assert.file([
-      mockPrompts.moduleName + '/' + mockPrompts.moduleName + '.info',
-      mockPrompts.moduleName + '/' + mockPrompts.moduleName + '.module',
-      mockPrompts.moduleName + '/' + mockPrompts.moduleName + '.install',
-      mockPrompts.moduleName + '/templates'
-    ]);
+  it('creates files and folders', function (done) {
+    runGenerator(function() {
+      assert.file([
+        mockPrompts.moduleName + '/' + mockPrompts.moduleName + '.info',
+        mockPrompts.moduleName + '/' + mockPrompts.moduleName + '.module',
+        mockPrompts.moduleName + '/' + mockPrompts.moduleName + '.install',
+        mockPrompts.moduleName + '/templates'
+      ]);
+      assert.fileContent(mockPrompts.moduleName + '/' + mockPrompts.moduleName + '.info',
+                           'dependencies[] = ' + testDependency);
+      done();
+    });
+  });
+
+  it('Creates files with the expected format and options', function(done) {
+    opts = {};
+    mockPrompts = {
+      moduleName: 'testModule2',
+      moduleDesc: 'Test creation of module',
+      hooks: [],
+      dependency: ''
+    };
+    runGenerator(function() {
+      assert.file([
+        mockPrompts.moduleName + '/' + mockPrompts.moduleName + '.info',
+        mockPrompts.moduleName + '/' + mockPrompts.moduleName + '.module'
+      ]);
+      assert.noFile([
+        mockPrompts.moduleName + '/' + mockPrompts.moduleName + '.install',
+        mockPrompts.moduleName + '/templates'
+      ]);
+      assert.noFileContent(mockPrompts.moduleName + '/' + mockPrompts.moduleName + '.info',
+                           'dependencies[]');
+      done();
+    });
   });
 });
