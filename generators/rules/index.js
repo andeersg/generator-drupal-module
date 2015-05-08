f688a334814d03a5d15c59b8564fc72f5ae4305e
Anders Grendstadbakk <anders.2205@gmail.com>
2015-05-08 15:18:06 +0200

    Add rules as a sub generator

---
+++ generators/rules/index.js
@@ -0,0 +1,13 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+
+module.exports = yeoman.generators.Base.extend({
+  writing: {
+    app: function () {
+      var context =  {
+        module_name: this.options.module_name
+      };
+      this.template('rules.inc', context.module_name +'/' + context.module_name + '.rules.inc', context);
+    }
+  }
+});
